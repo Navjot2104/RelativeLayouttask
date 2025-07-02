b3ac9579395f67368d1528ca31d485535f3db24e
Navjot2104 <gnavjotkaur572@gmail.com>
2025-07-02 17:38:25 +0530

    Update RecyclerActivity.kt

--- app/src/main/java/com/example/relativelayouttask/RecyclerActivity.kt
+++ app/src/main/java/com/example/relativelayouttask/RecyclerActivity.kt
@@ -5,11 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.relativelayouttask.databinding.ActivityMainBinding
 import com.example.relativelayouttask.databinding.RecyclerBinding
 
 class RecyclerActivity : AppCompatActivity() {
     lateinit var binding: RecyclerBinding
+    lateinit var recyclerAdapter: RecyclerAdapter
+    var Datalist= arrayListOf<recycledataclass>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,5 +23,13 @@ class RecyclerActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Datalist.add(recycledataclass(title="My title",description="My description"))
+        Datalist.add(recycledataclass(title="My title",description="MY description"))
+        Datalist.add(recycledataclass(title="My title",description="MY description"))
+recyclerAdapter=RecyclerAdapter(Datalist)
+        binding.recycler.layoutManager=LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        binding.recycler.adapter=recyclerAdapter
+        recyclerAdapter.notifyDataSetChanged()
+
     }
 }
