f556ac0097372035c392278d73ff5d176e5dbacd
Navjot2104 <gnavjotkaur572@gmail.com>
2025-07-02 17:38:22 +0530

    Update RecyclerAdapter.kt

--- app/src/main/java/com/example/relativelayouttask/RecyclerAdapter.kt
+++ app/src/main/java/com/example/relativelayouttask/RecyclerAdapter.kt
@@ -1,26 +1,46 @@
 package com.example.relativelayouttask
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Recycler
 
 class RecyclerAdapter (var list:ArrayList<recycledataclass>)
     :RecyclerView.Adapter<RecyclerAdapter.Viewholder>(){
     class Viewholder (var view: View):RecyclerView.ViewHolder(view) {
+        var name=view.findViewById<TextView>(R.id.txxtt)
+        var update=view.findViewById<Button>(R.id.btn123)
+        var delete=view.findViewById<Button>(R.id.btn234)
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Viewholder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.Viewholder {
+
+        var view=LayoutInflater.from(parent.context).inflate(R.layout.itemlayout,parent,false)
+        return Viewholder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list.size
+    }
+    interface OnClick{
+        fun update(position: Int)
+        fun delete(position: Int)
     }
 
-    override fun onBindViewHolder(holder: Viewholder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: RecyclerAdapter.Viewholder, position: Int) {
+        holder.apply {
+            name.setText(list[position].name)
+            update.setOnClickListener {
+                clickInterface.update(position)
+            }
+        }
+        delete.setOnClickListener{
+            clickInterface.delete(position)
+        }
     }
 
 
